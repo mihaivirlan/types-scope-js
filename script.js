c8bfd3ea55ba6d8061a851c028ecb0c678a9b2ff
mihail.virlan <mihail.virlan@esolutions.ro>
2020-05-09 00:07:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+    //Primitive Types
+// var myObj = {firstname : "Mihail", lastname : "Virlan"};
+// var arrayJson = JSON.stringify(myObj);
+// console.log(myObj);
+
+// var aNumber = 5;
+// console.log(aNumber);
+// var anotherNumber = aNumber;
+// console.log(anotherNumber);
+// aNumber = 12;
+// console.log(aNumber);
+// console.log(anotherNumber);
+
+// var array = [1, 2, 3];
+// var anotherArray = array;
+// console.log(array);
+// console.log(anotherArray);
+// array.push(4);
+// console.log(array);
+// console.log(anotherArray);
+
+
+
+    //Reference Types
+// var array = [1, 2, 3];
+// var anotherArray = array;
+// array = ['a', 'b'];
+// console.log(array);
+// console.log(anotherArray);
+
+
+
+    //Global Scope
+// var test = 'Global Scope';
+// function localScope () {
+//     console.log(test);
+// }
+// localScope();
+
+// var test = 'Global Scope';
+// function localScope () {
+//     var test = 'Local Scope';
+//     console.log(test);
+// }
+// localScope();
+// console.log(test);
+
+
+
+    //Local Scope
+// var test = 'Global Scope';
+// function localScope () {
+//     var test = 'Local Scope';
+//     console.log(test);
+// }
+// localScope();
+// console.log(test);
+
+// function localScope () {
+//     var test = 'Local Scope';
+//     console.log(test);
+// }
+// localScope();
+// console.log(test);
+
+// function localScope () {
+//     test = 'Local Scope';
+//     console.log(test);
+// }
+// localScope();
+// console.log(test);
+
+
